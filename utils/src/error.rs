c77b118a52da170706c580aff9f6ae15ec50b6ca
Jomocool <2512364506@qq.com>
2025-02-09 22:27:43 +0800

    implement mod utils

---
+++ utils/src/error.rs
@@ -0,0 +1,24 @@
+// ! # Errors
+// !
+// ! Custom Errors for the whole library.
+
+/////////////////////////////////////////////////////////////////////////////////
+
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum UtilsError {
+    #[error("Conversion error: {0}")]
+    ConversionError(String),
+
+    #[error("Could not create message: {0}")]
+    CreateMessage(String),
+
+    #[error("Error recovering key: {0}")]
+    RecoverError(String),
+
+    #[error("Error verifying signature: {0}")]
+    VerifyError(String),
+}
+
+pub type Result<T> = std::result::Result<T, UtilsError>;
