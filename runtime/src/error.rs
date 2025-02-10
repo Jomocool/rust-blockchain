deda4e969ab51a3cb1174dde0e0f139f11bf5036
Jomocool <2512364506@qq.com>
2025-02-10 22:58:36 +0800

    implement mod runtime

---
+++ runtime/src/error.rs
@@ -0,0 +1,28 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum RuntimeError {
+    #[error("Error invoking function {0}")]
+    CallFunctionError(String),
+
+    #[error("Error executing {0}")]
+    ExecutionError(String),
+
+    #[error("Error exporting function {0}")]
+    ExportFunctionError(String),
+
+    #[error("Invalid parameter type {0}")]
+    InvalidParamType(String),
+
+    #[error("Wasmtime error {0}")]
+    WasmtimeError(String),
+}
+
+/// Utility result type to be used throughout
+pub type Result<T> = std::result::Result<T, RuntimeError>;
+
+impl From<anyhow::Error> for RuntimeError {
+    fn from(error: anyhow::Error) -> Self {
+        RuntimeError::WasmtimeError(error.to_string())
+    }
+}
