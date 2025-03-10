583f9157d9dece2cc7edd083cb76eb9a9aaddcb6
许梓毫 <xuzihao@ft.tech>
2025-03-10 16:43:16 +0800

    补充proc_macros注释

--- proc_macros/src/lib.rs
+++ proc_macros/src/lib.rs
@@ -3,9 +3,15 @@ mod newtype;
 use proc_macro::TokenStream;
 use syn::parse_macro_input;
 
-
+/// 新类型派生宏
+///
+/// 该函数允许用户通过派生宏的方式定义新类型
+/// 它接受一个结构体或枚举类型，并为其生成额外的实现代码
+/// 主要用于简化新类型的创建过程，并自动实现一些常见的trait
 #[proc_macro_derive(NewType)]
-pub fn newtype(item: TokenStream) -> TokenStream{
+pub fn newtype(item: TokenStream) -> TokenStream {
+    // 解析输入的token流，将其转换为可以操作的数据结构
     let input = parse_macro_input!(item);
+    // 调用newtype::append函数处理输入，并将结果转换回token流
     newtype::append(input).into()
 }
--- proc_macros/src/newtype.rs
+++ proc_macros/src/newtype.rs
@@ -4,13 +4,34 @@ use proc_macro2::TokenStream as TokenStream2;
 use quote::quote;
 use syn::{parse2, DeriveInput, FieldsUnnamed};
 
+/**
+ * 实现一个过程宏，用于生成新类型结构体的Deref、DerefMut和Into trait的实现。
+ *
+ * # 参数
+ *
+ * - `input`: 一个`TokenStream2`，代表输入的Rust代码流，其中包含了一个derive输入结构体的定义。
+ *
+ * # 返回值
+ *
+ * - 返回一个`TokenStream2`，其中包含了生成的Rust代码流，即Deref、DerefMut和Into trait的实现。
+ *
+ * # 功能描述
+ *
+ * 此函数旨在为新类型结构体（例如`struct Block(SimpleBlock)`）生成常见的trait实现。
+ * 它首先解析输入的结构体定义，然后检查该结构体是否为新类型结构体（即只有一个未命名字段的结构体）。
+ * 如果是，它将为该结构体生成Deref、DerefMut和Into trait的实现，这些实现都指向内部的未命名字段。
+ * 如果输入的结构体不是新类型结构体，函数将触发一个panic，指出错误。
+ */
 pub fn append(input: TokenStream2) -> TokenStream2 {
+    // 解析输入的TokenStream2为DeriveInput结构体，以便获取结构体的标识符和数据结构。
     let DeriveInput { ident, data, .. } = parse2(input).unwrap();
+    // 构造一个错误消息，用于在结构体不符合新类型结构体要求时显示。
     let error = format!(
         "{} is not a new type struct (e.g. struct Block(SimpleBlock))",
         ident
     );
 
+    // 尝试从数据结构中提取未命名字段的标识符，如果结构体不是新类型结构体，则触发panic。
     let inner_ident = match data {
         syn::Data::Struct(s) => match s.fields {
             syn::Fields::Unnamed(FieldsUnnamed { unnamed, .. }) => unnamed,
@@ -19,7 +40,9 @@ pub fn append(input: TokenStream2) -> TokenStream2 {
         _ => panic!("{}", error),
     };
 
+    // 使用`quote` crate生成实现Deref、DerefMut和Into trait的代码。
     let output = quote! {
+        // 实现Deref trait，允许通过新类型结构体访问其内部的未命名字段。
         impl std::ops::Deref for #ident {
             type Target  = #inner_ident;
 
@@ -28,12 +51,14 @@ pub fn append(input: TokenStream2) -> TokenStream2 {
             }
         }
 
+        // 实现DerefMut trait，允许通过新类型结构体修改其内部的未命名字段。
         impl std::ops::DerefMut for #ident {
             fn deref_mut(&mut self) -> &mut #inner_ident {
                 &mut self.0
             }
         }
 
+        // 实现Into trait，允许将新类型结构体转换为其内部的未命名字段。
         impl Into<#inner_ident> for #ident {
             fn into(self) -> #inner_ident {
                 self.0
@@ -41,6 +66,7 @@ pub fn append(input: TokenStream2) -> TokenStream2 {
         }
     };
 
+    // 返回生成的代码作为TokenStream2。
     output
 }
 
